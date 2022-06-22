004a2c0476a6572cc9fa20cf687f9e6a48acc95c
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2022-06-23 00:26:11 +0530

    made some changes in the model structures and added the ordering meta attribute

--- enfuego/models.py
+++ enfuego/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 # model for the team
 class Teams(models.Model):
+    class Meta:
+        ordering = ['team']        # this is for setting the default ordering for the rows of the relation
+
     team = models.CharField(max_length=5, verbose_name="Team Name", blank=False, null=False, primary_key=True)
 
     def __str__(self):
@@ -13,6 +16,9 @@ class Teams(models.Model):
 
 # players name , team , goal scored, (player tag)
 class Players(models.Model):
+    class Meta:
+        ordering = ['-goalsscored']
+    
     name = models.CharField(max_length=25, verbose_name="Player Name", blank=False, null=False)
     team = models.ForeignKey(Teams, on_delete=models.CASCADE)
     goalsscored = models.PositiveIntegerField(verbose_name="Goals Scored")
@@ -24,25 +30,32 @@ class Players(models.Model):
 
 # model for all fixtures
 class Fixtures(models.Model):
+    class Meta:
+        ordering = ['matchnumber']
+
     matchnumber = models.PositiveIntegerField(verbose_name="match number")
     teama = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name="teama")
     teamb = models.ForeignKey(Teams, on_delete= models.CASCADE, related_name="teamb")
-    scorea = models.PositiveIntegerField(verbose_name="teama score")
-    scoreb = models.PositiveIntegerField(verbose_name="teamb score")
+    scorea = models.PositiveIntegerField(verbose_name="teama score", blank=True, null=True)
+    scoreb = models.PositiveIntegerField(verbose_name="teamb score", blank=True, null=True)
     date = models.DateField(verbose_name="match date")
     finished = models.BooleanField()
 
     def __str__(self):
-        return str(self.teama + "vs" + self.teamb)
+        return str(str(self.teama) + "vs" + str(self.teamb))
 
 
 # pointtable
 class PointTable(models.Model):
+    class Meta:
+        ordering = ['-points', '-goaldifference', '-goalsscored']
     team = models.OneToOneField(Teams, on_delete=models.CASCADE, primary_key=True)
     gamesplayed = models.PositiveIntegerField(verbose_name="games played")
     gameswon = models.PositiveIntegerField(verbose_name="games won")
     gamesdraw = models.PositiveIntegerField(verbose_name="games draw")
     gameslost = models.PositiveIntegerField(verbose_name="games lost")
+    goalsscored = models.PositiveIntegerField(verbose_name="goals scored")
+    goalsconceded = models.PositiveIntegerField(verbose_name="goals conceded")
     goaldifference = models.IntegerField(verbose_name="goal difference")
     points = models.PositiveIntegerField(verbose_name="points")
 
