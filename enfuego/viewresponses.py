34257bfc61fad898029e1056782a356749959ebc
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2022-06-23 06:29:23 +0530

    created viewresponses.py for getting resulting queryset to be passed to the views. It is done for pointtable, complete fixture, and goal scorers.

---
+++ enfuego/viewresponses.py
@@ -0,0 +1,36 @@
+from . import models
+from django.http import JsonResponse
+
+
+# result for pointtable. directly pass to JsonResponse with safe=False
+def point_table_response():
+    data = list(models.PointTable.objects.all().values())
+    
+    # data2 = JsonResponse(data, safe=False)
+
+    # print("-----------------", type(data))
+    # print(data)
+
+    # print("-----------------", type(data2))
+    # print(data2)
+
+    return data
+
+
+# results for complete fixtures.
+def complete_fixtures_response():
+    # data = list(models.Fixtures.objects.all().values())   # working but with "id"
+    data = list(models.Fixtures.objects.all().values('matchnumber', 'teama_id', 'teamb_id', 'scorea', 'scoreb', 'date', 'finished'))
+
+    # print("----------------", type(data))
+    # print(data)
+
+
+    return data
+
+
+# result for goal scorers
+def goal_scorers_response():
+    data = list(models.Players.objects.filter(goalsscored__gt=0).values('name', 'team_id', 'goalsscored'))
+    return data
+    
--- enfuego/views.py
+++ enfuego/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from . import models
+
+from . import viewresponses
 
 # Create your views here.
 
@@ -12,12 +14,24 @@ def point_table_view(request):
     # this is the landing page, check if all teams are there in the teams and pointtable database. Create a function for that purpose in models.py
     models.database_initialization()
 
-    return HttpResponse("this is point table view")
+    data = viewresponses.point_table_response()
+    """This returns an array of objects with keys { team_id, gamesplayed, gameswon, gamesdraw, 
+    gameslost, goalsscored, goalsconceded, goaldifference, points }"""
+
+    # return HttpResponse("this is point table view")
+    return JsonResponse(data, safe=False)
 
 
 # this view is for the page that shows the complete fixtures
 def complete_fixtures_view(request):
-    return HttpResponse("this is fixtures view")
+
+    data = viewresponses.complete_fixtures_response()
+    """This returns array of objects with keys {matchnumber, teama_id, teamb_id,
+    scorea, scoreb, date, finished}"""
+
+    # return HttpResponse("this is fixtures view")
+
+    return JsonResponse(data, safe=False)
 
 
 # this view is for the specific team fixtures
@@ -27,7 +41,12 @@ def specific_fixtures_view(request):
 
 # this view is for the table of goal scorers 
 def goal_scorers_view(request):
-    return HttpResponse("this is goal scorers view")
+
+    data = viewresponses.goal_scorers_response()
+    """returns array of objects with keys {name, team_id, goalsscored}"""
+    
+    # return HttpResponse("this is goal scorers view")
+    return JsonResponse(data, safe=False)
 
 
 # this view is for the guidelines
